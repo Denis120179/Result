246ae4dd96ebb4164124546f7666d98679880c7f
Denis <Denis120179@yandex.by>
2022-11-26 14:55:37 +0300

    программа к первой задаче

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Программа для первого задания
+
+Console.WriteLine("Введите количество раундов R: ");
+int R = int.Parse(Console.ReadLine()!);
+Game (R);
+
+void Game(int r)
+{
+    int a = 0;
+    int b = 0;
+    for (int count = 0; count < r; count ++) 
+    {
+        int X = new Random().Next(0, 11);
+        Console.WriteLine("Введите число N: ");
+        int N = int.Parse(Console.ReadLine()!);
+        if (N == X) a = a + 1;
+        else b = b + 1;            
+    }
+    Console.WriteLine($"Счет игры Пользователь {a} - {b} Компьютер"); 
+} 
+*/
